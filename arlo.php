c9c13359a00602d42cf7270753370e2acea083f2
Chelsea David <cryan17@cnm.edu>
2018-07-29 10:47:33 -0600

    created js file

---
+++ arlo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Senator Arlo</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css"
+</head>
+	<body>
+		<div class="cosmos">
+				<h1>Test</h1>
+			<p> Blah Blah Blah Blah Blah Blah</p>
+			<ul>
+				<li class="you">poop</li>
+				<li class="you">poop</li>
+			</ul>
+		</div>
+	</body>
+
+</html>
